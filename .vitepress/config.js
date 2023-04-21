12529e2622d9bacd0a73e5a6fd21ea9350e1881f
chilin99999 <chilin99999@gmail.com>
2023-04-21 22:20:48 +0800

    Initial commit

---
+++ .vitepress/config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vitepress';
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: '簡評桌遊',
+  description: '簡單扼要聊聊桌遊',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [{ text: '桌遊列表', link: '/' }],
+
+    sidebar: [
+      {
+        text: '輕策',
+        items: [
+          { text: '得分沙拉', link: '/light/point-salad' },
+          { text: '香料之路', link: '/light/snorta' },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+    ],
+  },
+});
---
+++ light/point-salad.md
@@ -0,0 +1,9 @@
+# 得分沙拉
+
+This page demonstrates some of the built-in markdown extensions provided by VitePress.
+
+## BGG
+
+7.2
+
+[Point-salad](https://boardgamegeek.com/boardgame/274960/point-salad)
